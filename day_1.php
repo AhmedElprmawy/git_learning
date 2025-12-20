531a0cd5b0f23f39fa2fdfbad583a85274fa1875
Ahmed Alprmawy <Ahmedelprmawy236@gmail.com>
2025-12-20 16:06:30 +0200

    day 2: conditions & loops

---
+++ day_1.php
@@ -0,0 +1,49 @@
+<?php 
+//  day_1
+// $name = "ahmed";
+// $age = 25;
+// $carer= "developer";
+
+// if($age > 18){
+//     echo "allowed";
+
+// }elseif($age > 18 && $carer=="devoloper"){
+//     echo "allowed for devolper";
+
+// }else{
+//     echo "not allowed";
+// }
+
+//day_2
+
+// echo "<br>";
+
+// $count="";
+// for($i=0; $i<=20; $i++){
+//     if($i % 2 == 0){
+
+//         echo $i;
+//     }
+//     echo "<br>";
+// }
+
+// for($i=1; $i<=20; $i++){
+
+//     if($i % 3==0){
+//         echo "Fizz <br>"; 
+//     }else{
+//         echo $i."<br>";
+//     }
+
+// }
+
+// $name = "admin";
+
+// if($name == "admin"){
+//     echo "welcome admin";
+// }else{
+//     echo "access denied";
+// }
+
+
+?>
