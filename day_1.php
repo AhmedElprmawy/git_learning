6aec6f97a07fe0558a136e942726e7d427dfb3ba
Ahmed Alprmawy <Ahmedelprmawy236@gmail.com>
2025-12-24 21:25:15 +0200

    day 4 : array

--- day_1.php
+++ day_1.php
@@ -70,4 +70,38 @@
 // echo $count;
 
 
+// day_4
+
+// $students = array("ahmed","mohamed","anas","sara","hend");
+
+
+
+// foreach($students as $student){
+//     echo $student . "<br>";
+// }
+
+
+
+// foreach($students as &$student){
+//     if($student == "anas"){
+//         echo "top student : " . $student . "<br>";
+//     }else{
+//         echo $student . "<br>";
+//     }
+// }
+// unset($student);
+
+
+$students = [["name"=>"ahmed","grade"=>90],["name"=>"mohamed","grade"=>75],["name"=>"anas","grade"=>60]];
+
+foreach($students as $student){
+    if($student["grade"] >=85){
+        echo $student["name"] . " : excellent <br>";
+    }elseif($student["grade"] >65){
+        echo $student["name"] . " : good <br>";
+        }else{
+            echo $student["name"] . " : fail <br>";
+        }
+}
+
 ?>
